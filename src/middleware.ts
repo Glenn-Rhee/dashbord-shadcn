6b56e81243c647d6d54daa77695990d4d9485330
Glenn-Rhee <glennviktor5@gmail.com>
2024-01-25 21:07:56 +0700

    Menghapus root path pada middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,5 +13,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/order", "/settings", "/users", "/auth/:path*"],
+  matcher: ["/order", "/settings", "/users", "/auth/:path*"],
 };
