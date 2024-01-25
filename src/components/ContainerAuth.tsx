e5c8d8bf167f10f7bbff26b91e8c9c97b05a7167
Glenn-Rhee <glennviktor5@gmail.com>
2024-01-25 21:33:24 +0700

    Memperbaiki UI

--- src/components/ContainerAuth.tsx
+++ src/components/ContainerAuth.tsx
@@ -5,7 +5,7 @@ export default function ContainerAuth(props: ContainerAuthProps) {
   const { children } = props;
   return (
     <div className="flex w-full justify-center items-center p-4">
-      <CardContent className="w-3/4 md:w-[60%] lg:w-[40%] shadow-2xl">
+      <CardContent className="w-full md:w-[60%] lg:w-[40%] shadow-2xl">
         {children}
       </CardContent>
     </div>
